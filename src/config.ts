bb50c787083772e81bef697d85eee20484377f8e
Alejandro Sergio D'Alessandro <admin@epa-bienestar.com>
2026-06-08 20:59:06 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@
 
 // Default values work on localhost:3000 and foomedical.com
 // Replace these values with your own values for production
-export const MEDPLUM_PROJECT_ID = '9602358d-eeb0-4de8-bccf-e2438b5c9162';
-export const MEDPLUM_GOOGLE_CLIENT_ID = '679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com';
-export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfFd_8gAAAAAOCVrZQ_aF2CN5b7s91NEYIu5GxL';
+export const MEDPLUM_PROJECT_ID = '97c26210-36a1-43aa-8dc4-2215b29c6709';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '472653584585-r7qqen8apn46mtdop6fdsog0ojllgsmu.apps.googleusercontent.com';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LclqvMsAAAAAFKkDSOS0rMCFrMyUdm9iuMMM5_2';

a98432e61eceefdcf6e19fafac0582351fecb700
Alejandro Sergio D'Alessandro <admin@epa-bienestar.com>
2026-06-15 15:15:50 -0300

    Remove unused import of Link in Header.tsx

--- src/pages/landing/Header.tsx
+++ src/pages/landing/Header.tsx
@@ -35,7 +35,6 @@ import type { JSX } from 'react';
 import { useNavigate } from 'react-router';
 import { Logo } from '../../components/Logo';
 import classes from './Header.module.css';
-import { useNavigate, Link } from 'react-router';
 
 const programFeatures = [
   {

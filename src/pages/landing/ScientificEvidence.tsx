c74af8b6a0ec5d3db235f6f6b7c0a5a311b4eed6
Alejandro Sergio D'Alessandro <admin@epa-bienestar.com>
2026-06-15 18:21:18 -0300

    Update ScientificEvidence.tsx

--- src/pages/landing/ScientificEvidence.tsx
+++ src/pages/landing/ScientificEvidence.tsx
@@ -78,7 +78,7 @@ export function ScientificEvidence(): JSX.Element {
                     <Image 
                       src={pillar.imgSrc} 
                       alt={`Gráfico de ${pillar.title}`} 
-                      fallbackSrc="src/img/landing/le8.png"
+                      fallbackSrc="./src/img/landingPage/le8.png"
                     />
                   </Card>
                 </Grid.Col>
